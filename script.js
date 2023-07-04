4754782499ecd63956b3e8652db18a0c1d268018
Emmanuel <superkiko80@hotmail.com>
2023-07-04 12:39:20 -0600

    clase 02 terminada

---
+++ script.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector("[data-form-btn]");
+
+const createTask = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector("[data-form-input]");
+    console.log(input.value);
+}
+
+
+console.log(btn);
+
+
+//arrow functions o funciones anonimas
+btn.addEventListener("click", createTask);
